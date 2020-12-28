0e38173e5145ad63459ad7060323cdeefa2bdde1
JINHO SON <a1234@1234s-MacBook-Pro.local>
2020-12-28 20:15:23 +0900

    concept of ts

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Abroad", year = 2021, country = "philippines", city = "metro manila";
+const sayGo = (name, year, country, city) => {
+    console.log(`i will prepare to move to  ${city} in the ${country} in ${year}`);
+};
+sayGo(name, year, country, city);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("hello");
+const name = "Abroad",
+    year = 2021,
+    country = "philippines",
+    city = "metro manila";
+
+const sayGo = (name, year, country, city) => {
+    console.log(`i will prepare to move to  ${city} in the ${country} in ${year}`)
+};
+
+sayGo(name, year, country, city);
+
+export {};
