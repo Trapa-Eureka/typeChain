15bf226866f867749e6c6b40a6378e8acbf63269
JINHO SON <a1234@1234s-MacBook-Pro.local>
2020-12-28 23:30:04 +0900

    create concept blockchain

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,28 @@
-class immigrate {
-    public year: number;
-    public country: string;
-    public city: string;
-    constructor(year: number, country: string, city: string) {
-        this.year = year;
-        this.country = country;
-        this.city = city;
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor (
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
 
-const abroad = new immigrate(2021, "Philippines", "Metro Manila");
+const abroadBlock: Block = new Block(0, "313131313131", "", "flight", 123456789);
 
-const sayGo = (abroad: immigrate): string => {
-    return `i will prepare to move to ${abroad.city} in the ${abroad.country} in ${abroad.year}!`;
-};
+let blockchain: [Block] = [abroadBlock];
 
-console.log(sayGo(abroad));
+console.log(blockchain);
 
 export {};
