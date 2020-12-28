17db545c1d1030ec3968b363b45d868eac1b6e5e
JINHO SON <a1234@1234s-MacBook-Pro.local>
2020-12-28 22:54:55 +0900

    concept of Class on TS

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-interface immigrate {
-    year: number;
-    country: string;
-    city: string;
+class immigrate {
+    public year: number;
+    public country: string;
+    public city: string;
+    constructor(year: number, country: string, city: string) {
+        this.year = year;
+        this.country = country;
+        this.city = city;
+    }
 }
 
-const abroad = {
-    year: 2021,
-    country: "philippines",
-    city: "metro manila"
-};
+const abroad = new immigrate(2021, "Philippines", "Metro Manila");
 
 const sayGo = (abroad: immigrate): string => {
     return `i will prepare to move to ${abroad.city} in the ${abroad.country} in ${abroad.year}!`;
